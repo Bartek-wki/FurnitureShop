29679e29ed6961f73beb0afb9db23ff33488fb00
Justyna KUJAWA <justy.kujawa@gmail.com>
2022-03-29 22:16:13 +0200

    Chatbox

--- src/components/views/Homepage/Homepage.js
+++ src/components/views/Homepage/Homepage.js
@@ -6,12 +6,14 @@ import styles from './Homepage.module.scss';
 import FeatureBoxes from '../../features/FeatureBoxes/FeatureBoxes';
 import NewFurniture from '../../features/NewFurniture/NewFurnitureContainer';
 import CompareBar from '../../features/CompareBar/CompareBar';
+import Chatbox from '../../features/Chatbox/Chatbox';
 
 const Homepage = () => (
   <div className={styles.root}>
     <FeatureBoxes />
     <NewFurniture />
     <CompareBar />
+    <Chatbox />
   </div>
 );
 
